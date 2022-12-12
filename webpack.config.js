f095412f3f4bb1e26a2f57900801190e55012f7c
Nikolai_Schmidt <nikolai.schmidt@create-3d.de>
2022-12-12 17:32:17 +0700

    changed dist to docs for compatibility with github pages

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     plugins: [
         new CopyPlugin({
